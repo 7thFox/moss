34131809e4dda02d01a2021b142a98ec28621ed6
7thFox <dev7thFox@gmail.com>
2020-09-07 14:33:23 -0500

    Reconfigure Rust build/linking so std libs are included

--- kernel/main.rs
+++ kernel/main.rs
@@ -1,16 +1,16 @@
-
 #[no_mangle]
 fn main() {
-    unsafe {
-        let video_memory_addr = 0xb8000;
-        let video_memory = video_memory_addr as *mut u8;
-        *video_memory = 'X' as u8;
-        *video_memory.add(1) = 'Y' as u8;
-        *video_memory.add(2) = 'Z' as u8;
-        *video_memory.add(3) = 'A' as u8;
-//        *video_memory = 0x0F;
-//        *video_memory.add(1) = 'X' as u8
-    }
+//    write_char('A', 0, 0x0);
+    loop {}
 }
 
+fn write_char(c: char, pos: u32, attr: u8) {
+    unsafe {
+        let addr = 0xb8000 + (pos * 2);
+        let c_ptr = addr as *mut u8;
+        let a_ptr = (addr + 1) as *mut u8;
 
+        *c_ptr = c as u8;
+        *a_ptr = attr;
+    }
+}
